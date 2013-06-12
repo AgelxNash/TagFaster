1263e9a4b277eb0d6d22038486311eaad4fe1831
AgelxNash <iam@agel-nash.ru>
2013-06-12 17:57:40 +0600

    Fix NOP

--- core/components/tagfaster/model/tagfaster/tags.class.php
+++ core/components/tagfaster/model/tagfaster/tags.class.php
@@ -1,3 +1,13 @@
 <?php
+include_once(dirname(dirname(dirname(__FILE__)))."/xnop.class.php");
+
 class Tags extends xPDOSimpleObject {
+	public static function load(xPDO & $xpdo, $className, $criteria, $cacheFlag= true) {
+		$instance = parent::load($xpdo,__CLASS__,$criteria,$cacheFlag);
+		if(!is_object($instance) || !($instance instanceof $className)){
+			$instance = new xNop;
+			$xpdo->log(xPDO::LOG_LEVEL_DEBUG,'Load NOPing class for ' . __CLASS__ ." WHERE ".print_r($criteria,1));
+		}
+		return $instance;
+	}
 }
